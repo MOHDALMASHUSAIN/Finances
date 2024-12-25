bdcfb341fdc89812185ce1d30720ab7c37d06d6e
MOHD ALMAS HUSAIN <iconalmas@gmail.com>
2024-12-25 15:20:57 +0530

    add files via upload

--- Python/M1D1BasicPython.py
+++ Python/M1D1BasicPython.py
@@ -1,1 +1,19 @@
 print("almas")
+print("almas is the craziest mofo")
+a= 12
+b=32
+print(a+b)
+x=str(12)
+x=18
+print(x)
+x=int(12)
+print(x)
+z= float(12)
+print(z)
+k= 14
+print(type(z))
+x,y,z="kevin",4,float(19)
+print(z)
+print(x)
+print(y)
+print(a,b,x)
---
+++ Python/M1D2Operator.py
@@ -0,0 +1,9 @@
+a = 12
+b=3
+print(a+b,a-b,a*b,a%b,a**b,b**b,a/b)
+print(a==b,a!=b)
+print(a>b)
+
+
+
+
