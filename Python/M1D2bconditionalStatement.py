0c4640b46c99ce2d89d22ae633821e0dcb03e52d
MOHD ALMAS HUSAIN <iconalmas@gmail.com>
2024-12-25 17:10:34 +0530

    add files via upload

---
+++ Python/M1D2bconditionalStatement.py
@@ -0,0 +1,10 @@
+a=23
+if a>76 :print("false")
+elif a<2:
+    print("true")
+else:
+    print("coolest dude id almas" , a)
+
+
+
+
---
+++ Python/M1D2croiCalculation.py
@@ -0,0 +1,5 @@
+buy = float(input("Give the cost of  bought price"))
+sold = float(input("give the selling price"))
+profit = sold-buy
+ROI= (profit/buy)*100
+print("The return on Investment is " , ROI)
